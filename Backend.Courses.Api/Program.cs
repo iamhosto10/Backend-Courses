d9148cd41df9deaec8eae1b650e945a2ad8e4fc9
Gerardo Ramirez <gerardoramirez4400@gmail.com>
2025-09-26 07:47:44 -0500

    add cors to program.cs

--- Backend.Courses.Api/Program.cs
+++ Backend.Courses.Api/Program.cs
@@ -53,6 +53,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
 
 // =====================
